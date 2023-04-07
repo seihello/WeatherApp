20f7ccec2004cd4b9d1de30d61a5b048bbdb8ebc
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-04-07 16:56:28 -0700

    Get the longitude and latitude of a city by using Geocoding API

--- current.js
+++ current.js
@@ -4,3 +4,19 @@ function initMap() {
 
     autocomplete = new google.maps.places.Autocomplete(searchCityInput);
 }
+
+
+const apiKey = "c5b83392add58be24fb5a7bd362ced83"
+let cityName = "Vancouver"
+const limit = 1
+let requestURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${apiKey}`
+
+
+fetch(requestURL).then((responce) => {
+    responce.json().then((data) => {
+        console.log(data)
+    })
+})
+.catch((error) => {
+    console.log("Fetch Error: " + error)
+})
