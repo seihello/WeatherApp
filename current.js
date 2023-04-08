5eba8a36262ca3c137a58fef5aa005efa4829189
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-04-08 00:07:39 -0700

    Make a function for showing the weather fetched from API

--- current.js
+++ current.js
@@ -12,38 +12,40 @@ function initMap() {
     });
 }
 
-
-let currentWeatherElement = document.querySelector("#weather-info")
+const currentWeatherElement = document.querySelector("#weather-info")
 
 const apiKey = "c5b83392add58be24fb5a7bd362ced83"
-let cityName = "Vancouver"
 const limit = 1
-let locationRequest = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${apiKey}`
 
-let lat = 0
-let lon = 0
-fetch(locationRequest).then((response) => {
-    return response.json()
-})
-.then((data) => {
-    let location = data[0]
-    lat = location["lat"]
-    lon = location["lon"]
-    console.log(lat)
-    console.log(lon)
+showWeather("Vancouver")
 
-    let weatherRequest = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+function showWeather(cityName) {
+    
+    const locationRequest = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${apiKey}`
 
-    return fetch(weatherRequest)
-})
-.then((response) => {
-    return response.json()
-})
-.then((data) => {
-    console.log(data)
-
-    currentWeatherElement.innerHTML = data["weather"][0]["main"]
-})
-.catch((error) => {
-    console.log("Fetch Error: " + error)
-})
+    let lat = 0
+    let lon = 0
+    fetch(locationRequest).then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        let location = data[0]
+        lat = location["lat"]
+        lon = location["lon"]
+    
+        const weatherRequest = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    
+        return fetch(weatherRequest)
+    })
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data)
+    
+        currentWeatherElement.innerHTML = data["weather"][0]["main"]
+    })
+    .catch((error) => {
+        console.log("Fetch Error: " + error)
+    })
+}
