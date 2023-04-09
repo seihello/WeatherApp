4fc09ad41919e6544cdfe92da9930eae6d624158
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-04-08 22:39:09 -0700

    Remove the selected city from the favorite cities when the star is unselected

--- current.js
+++ current.js
@@ -9,11 +9,52 @@ function initMap() {
     });
 }
 
+
+// Favorite Cities
+let favoriteCities = []
+
+const selectedCityNameElement = document.querySelector("#city-name")
+const favoriteStarElement = document.querySelector("#favorite-star")
+favoriteStarElement.addEventListener("click", onFavoriteStarClicked)
+
+const favoriteCitiesMenu = document.querySelector("#favorite-cities")
+
+function onFavoriteStarClicked() {
+    const selectedCity = selectedCityNameElement.innerHTML
+
+    if(!favoriteCities.includes(selectedCity)) {
+        // Add to the list of the favorite cities
+        favoriteCities.push(selectedCity)
+
+        // Add to the pull-down menu
+        const newFavoriteCityOption = document.createElement("option")
+        newFavoriteCityOption.innerHTML = selectedCity
+        favoriteCitiesMenu.appendChild(newFavoriteCityOption)
+    } else {
+        let index = favoriteCities.indexOf(selectedCity)
+
+        // Remove the selected city from the favorite cities
+        favoriteCities.splice(index, 1)
+
+        // Remove the selected city from the pull-down menu
+        favoriteCitiesMenu.removeChild(favoriteCitiesMenu.children[index+1])
+    }
+
+    setFavoriteStar()
+}
+
+function setFavoriteStar() {
+    if(favoriteCities.includes(selectedCityNameElement.innerHTML)) {
+        favoriteStarElement.src = "img/star-on.png"
+    } else {
+        favoriteStarElement.src = "img/star-off.png"
+    }
+}
+
 // Current Weather
 const apiKey = "c5b83392add58be24fb5a7bd362ced83"
 const defaultCity = "Vancouver"
 
-const selectedCityNameElement = document.querySelector("#city-name")
 const currentTemperatureElement = document.querySelector("#current-temperature")
 const currentWeatherElement = document.querySelector("#current-weather")
 
@@ -29,37 +70,17 @@ function showWeather(cityName) {
         return response.json()
     })
     .then((data) => {
-        console.log(data)
-    
         selectedCityNameElement.innerHTML = cityName
         currentTemperatureElement.innerHTML = data["main"]["temp"]
         currentWeatherElement.innerHTML = data["weather"][0]["main"]
+
+        setFavoriteStar()
     })
     .catch((error) => {
         console.log("Fetch Error: " + error)
     })
 
+
+
     // Call the function to display the weather of the next 5 days
-}
-
-// Favorite Cities
-let favoriteCities = []
-
-const favoriteStarElement = document.querySelector("#favorite-star")
-favoriteStarElement.addEventListener("click", onFavoriteStarClicked)
-
-const favoriteCitiesMenu = document.querySelector("#favorite-cities")
-
-function onFavoriteStarClicked() {
-    // Add to the list of the favorite cities
-    const newFavoriteCity = selectedCityNameElement.innerHTML
-    if(favoriteCities.includes(newFavoriteCity)) {
-        return
-    }
-    favoriteCities.push(newFavoriteCity)
-
-    // Add to the pull-down menu
-    const newFavoriteCityOption = document.createElement("option")
-    newFavoriteCityOption.innerHTML = newFavoriteCity
-    favoriteCitiesMenu.appendChild(newFavoriteCityOption)
 }
